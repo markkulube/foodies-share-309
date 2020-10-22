1f7e0053d291aa05db39c4bd4d9be199400420df
EddieShao <shao.eddie@gmail.com>
2020-10-22 10:16:47 -0700

    feat: added documentation for required props to Recipe component

--- client/src/components/Recipe/Recipe.js
+++ client/src/components/Recipe/Recipe.js
@@ -5,6 +5,15 @@ import { UnmountClosed } from "react-collapse";
 // stylesheet
 import "./Recipe.css";
 
+/**
+ * A written recipe.
+ *
+ * Required props:
+ *  - title (string): The title of the recipe.
+ *  - desc (string): The summary description of the recipe shown at the top of the recipe.
+ *  - ingredients (string[]): A list of ingredients required.
+ *  - steps (string[]): The list of instructions to follow. The array indices indicate the order of instructions.
+ */
 export default class Recipe extends React.Component {
 
     constructor(props) {
