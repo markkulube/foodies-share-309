50b8fa294086d6fd6bed089af4b880338638252b
bjozwik <45638753+bjozwik@users.noreply.github.com>
2020-10-21 21:38:27 -0400

    feat: added Home page to App
    Home page is added with two buttons that will lead to a sign up page and login page.

---
+++ client/src/react-components/SignUp/index.js
@@ -0,0 +1,17 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import Button from "@material-ui/core/Button";
+
+import "./styles.css";
+
+/* Component for the Home page */
+class SignUp extends React.Component {
+  render() {
+    return (
+      <div>
+      </div>
+    );
+  }
+}
+
+export default SignUp;
