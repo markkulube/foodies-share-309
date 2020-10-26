ccf9ebc1ccadb89c8e5c23d7833485585ae23d00
EddieShao <shao.eddie@gmail.com>
2020-10-26 00:13:13 -0700

    feat: track input in state

--- client/src/components/ReviewList/index.js
+++ client/src/components/ReviewList/index.js
@@ -13,8 +13,16 @@ export default class Index extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            currentRating: 0  // the rating of the current in-progress review
+            currentRating: 0,  // rating of the current in-progress review
+            reviewContent: ""  // content of the current in-progress review
         }
+    }
+
+    /**
+     * Update the review content state with whatever is in the <textarea> input.
+     */
+    handleContentUpdate = (event) => {
+        this.setState({ reviewContent: event.target.value });
     }
 
     render() {
@@ -25,7 +33,8 @@ export default class Index extends React.Component {
                 <div>  {/* the block to write a review */}
                     <img id={"profile"} src={profile} alt={"profile"}/>
                     <div>
-                        <textarea rows={8} cols={30} placeholder={"Write a review"}/>
+                        <textarea onChange={this.handleContentUpdate} rows={8} cols={30}
+                                  placeholder={"Write a review"}/>
                         <Stars rating={this.state.currentRating} updateStar={updateStar} parent={this}/>
                         <button>Post</button>
                     </div>
