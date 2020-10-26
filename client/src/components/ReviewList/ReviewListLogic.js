55fb1714d53e31a2ce02dde29e96c028bfd17814
EddieShao <shao.eddie@gmail.com>
2020-10-26 01:48:20 -0700

    feat: added Reviews to ReviewList
    Added list of Reviews to ReviewList and implemented adding a new Review

--- client/src/components/ReviewList/ReviewListLogic.js
+++ client/src/components/ReviewList/ReviewListLogic.js
@@ -1,6 +1,36 @@
 /* Logic files for the ReviewList component. */
 
+// TODO: Mock data - remove once API calls are implemented in handlePostReview
+const reviews = [
+    {name: "Eddie Shao", content: "This is the review body.", rating: 4},
+    {name: "Mark Kulube", content: "This dish is seriously lacking some taste...", rating: 2},
+    {name: "Brandon Jozwik", content: "Wow, this dish is amazing. Please lend me the recipe!", rating: 5}
+]
+
+/**
+ * Update the stars displayed to the ReviewList with the given rating.
+ *
+ * @param {ReviewList}  reviewList  The ReviewList to update.
+ * @param {int}         rating      The rating to update to.
+ */
 export const updateStar = (reviewList, rating) => {
     console.log("update current rating to", rating);
     reviewList.setState({ currentRating: rating });
+}
+
+/**
+ * Post a new review for the given post under the given user name with the given content and rating. Update the given
+ * ReviewList with the new post.
+ *
+ * @param {ReviewList}  reviewList  The ReviewList to update.
+ * @param {string}      name        Name of the review writer.  TODO: POST name depending on how Keren passes it in
+ * @param {TBD}         post        The post the review is for.  TODO: change TBD to what Keren decides to pass in
+ * @param {string}      content     The content body of the review.
+ * @param {int}         rating      The rating of the review.
+ */
+export const handlePostReview = (reviewList, name, post, content, rating) => {
+    console.log(name, "is posting a", rating, "star review");
+    // TODO: implement function with mock API call
+    reviews.unshift({name: "\<current user\>", content: content, rating: rating});
+    reviewList.setState({reviews: reviews});
 }
--- client/src/components/ReviewList/index.js
+++ client/src/components/ReviewList/index.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { uid } from "react-uid";
 import Stars from "./Stars";
 import Review from "./Review"
 
@@ -7,15 +8,24 @@ import "./style.css";
 import profile from "../../images/eggie.jpg";
 
 /* import logic */
-import { updateStar } from "./ReviewListLogic";
+import { updateStar, handlePostReview } from "./ReviewListLogic";
 
-export default class Index extends React.Component {
+export default class ReviewList extends React.Component {
 
     constructor(props) {
         super(props);
+
+        // Note: calling handleFilter (aka. calling setState) in constructor somehow doesn't update the state.
+        //  Therefore, we need to make a manual API call to retrieve initial list of reviews.
+        const initReviews = [  // TODO: replace this with API call to retrieve reviews
+            {name: "Eddie Shao", content: "This is the review body.", rating: 4},
+            {name: "Mark Kulube", content: "This dish is seriously lacking some taste...", rating: 2},
+            {name: "Brandon Jozwik", content: "Wow, this dish is amazing. Please lend me the recipe!", rating: 5}
+        ]
         this.state = {
             currentRating: 0,  // rating of the current in-progress review
-            reviewContent: ""  // content of the current in-progress review
+            content: "",  // content of the current in-progress review
+            reviews: initReviews  // list of existing reviews
         }
     }
 
@@ -23,10 +33,12 @@ export default class Index extends React.Component {
      * Update the review content state with whatever is in the <textarea> input.
      */
     handleContentUpdate = (event) => {
-        this.setState({ reviewContent: event.target.value });
+        this.setState({ content: event.target.value });
     }
 
     render() {
+        const { name, post } = this.props;  // obtain the name of the reviewer and the post they are reviewing
+
         return(
             <div id={"review-list-container"}>
                 <button>close</button>
@@ -40,11 +52,23 @@ export default class Index extends React.Component {
                         <textarea onChange={this.handleContentUpdate} rows={4} cols={30}
                                   placeholder={"Write a review"}/>
                         <Stars rating={this.state.currentRating} updateStar={updateStar} parent={this}/>
-                        <button>Post</button>
+                        <button onClick={() =>
+                            handlePostReview(this, name, post, this.state.content, this.state.currentRating)}>
+                            Post
+                        </button>
                     </div>
                 </div>
-                {/* TODO: replace with map onto list of reviews (eventually from API call) */}
-                <hr/><Review name={"Eddie Shao"} content={"This is the body of the review"} rating={4}/>
+                <hr/>
+                {
+                    this.state.reviews.map(review => {
+                        return (
+                            <div key={uid(review)}>
+                                <Review name={review.name} content={review.content} rating={review.rating}/>
+                                <hr/>
+                            </div>
+                        );
+                    })
+                }
             </div>
         );
     }
