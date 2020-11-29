4fde6243812bfc98c166d4a7afaa16baf1397f84
bjozwik <brandon.jozwik@mail.utoronto.ca>
2020-11-28 22:24:56 -0500

    Added files to set up Express server

---
+++ server.js
@@ -0,0 +1,32 @@
+/* server.js for react-express-authentication */
+"use strict";
+const log = console.log;
+
+const express = require("express");
+// starting the express server
+const app = express();
+const path = require('path')
+
+/*** Webpage routes below **********************************/
+// Serve the build
+app.use(express.static(path.join(__dirname, "/client/build")));
+
+// All routes other than above will go to index.html
+app.get("*", (req, res) => {
+    // check for page routes that we expect in the frontend to provide correct status code.
+    const goodPageRoutes = ["/"];
+    if (!goodPageRoutes.includes(req.url)) {
+        // if url not in expected page routes, set status to 404.
+        res.status(404);
+    }
+
+    // send index.html
+    res.sendFile(path.join(__dirname, "/client/build/index.html"));
+});
+
+/*************************************************/
+// Express server listening...
+const port = process.env.PORT || 5000;
+app.listen(port, () => {
+    log(`Listening on port ${port}...`);
+});
