cf5a7c659a042721d9e9c867eb08c42094f66724
Keren Yu <keren.yu@mail.utoronto.ca>
2020-12-08 13:26:56 +0800

    update database

--- db/mongoose.js
+++ db/mongoose.js
@@ -5,7 +5,7 @@ const mongoose = require('mongoose');
 
 // Define the URI for the database.
 // If URI is not defined for deployment, use our local one for development.
-const mongoURI = process.env.MONGODB_URI || 'mongodb://localhost:27017/FoodiesAPI';
+const mongoURI = process.env.MONGODB_URI || 'mongodb+srv://user:user@foodies-dev.5h6a5.mongodb.net/<dbname>?retryWrites=true&w=majority';
 mongoose.connect(mongoURI, { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true});
 
 module.exports = { mongoose };  // Export the active connection.
